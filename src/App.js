9eaa968f0cf0bc471aae0ba1e165b2067f6c88d6
Jonathan Pay <jonathanpay@users.noreply.github.com>
2025-05-20 21:31:18 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Teleprompter from './components/Teleprompter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Teleprompter />
+    </div>
+  );
+}
+
+export default App;
